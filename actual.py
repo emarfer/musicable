59aeecdcb29c20002064cf0603b8dc00264508e4
emarfer <estermarferironhack@gmail.com>
2022-02-09 16:24:38 +0100

    ida de olla 🍲

--- actual.py
+++ actual.py
@@ -1,24 +1,43 @@
 import os
 from IPython.display import display
+import pandas as pd
+import time
 
 import src.apilast as als
 import src.sqltools as sqt
 import src.cleansing as cls
 
 
+
 keylast = os.getenv("keylast")
 l_user = input('enter lastfm user: ')
+if l_user.lower() != 'sinatxester':
+    print(f"has escrito '{l_user.upper()}', seguro que querías poner 'SINATXESTER', vamos a runear esto como si lo hubieras escrito bien")
+    l_user = 'sinatxester'
+elif l_user.lower() == 'sinatxester':
+    print('muy bien, aprendise a escribir')
+time.sleep(2)
 limit = 1000
 lastuts = sqt.maxuts()
 
 dating = cls.utslocal(lastuts)
 
 print(f'recovering scrobbles since {dating}')
+time.sleep(2)
 
 recenttracks = als.req_lastfm (l_user,limit,lastuts)
 
-for i,r in recenttracks.iterrows():
-    sqt.insert_data(r.uts, r.artist, r.artist_mbid, r.album, r.album_mbid, r.title, r.track_mbid)
+if type(recenttracks) == pd.DataFrame:
+    print(f'ready to insert {len(recenttracks)} new tracks')
+else:
+    print(recenttracks)
+time.sleep(2)
 
-insertadas = sqt.insert_scro()
-display(insertadas)
+
+if type(recenttracks) == pd.DataFrame:
+    for i,r in recenttracks.iterrows():
+        sqt.insert_data(r.uts, r.artist, r.artist_mbid, r.album, r.album_mbid, r.title, r.track_mbid)
+
+    insertadas = sqt.act_scro()
+    display(insertadas)
+
--- src/sqltools.py
+++ src/sqltools.py
@@ -2,6 +2,8 @@ import pandas as pd
 import os
 import sqlalchemy as alch
 import dotenv
+import datetime
+import time
 
 dotenv.load_dotenv()
 
@@ -9,6 +11,21 @@ passw = os.getenv("mysql")
 dbName = "musicablecero"
 connectionData = f"mysql+pymysql://root:{passw}@localhost/{dbName}"
 engine = alch.create_engine(connectionData)
+
+
+
+
+def utslocal (uts):
+    '''
+    recibe una cadena de texto o interger que es uts(unix time stamp, valor en segundos de una fecha)
+        unix timestamp count = This count starts at the Unix Epoch on January 1st, 1970 at UTC
+    devuelve fecha y hora local. 
+    '''
+    if type(uts) == int:
+        return datetime.datetime.fromtimestamp(uts).strftime('%Y-%m-%d %H:%M:%S')
+    else:
+        return datetime.datetime.fromtimestamp(int(uts)).strftime('%Y-%m-%d %H:%M:%S')
+
 
 def maxuts():
     '''
@@ -33,12 +50,14 @@ def insert_data(uts, artist, artist_mbid, album, album_mbid, title, track_mbid):
     recibe los datos del datframe creado con la api de ultimos tracks de lastfm
     inserta los datos en la tabla scrobbling de mysql
     '''
+    fechahora = utslocal(uts)
     
+
     if '"' not in str(artist) and '"' not in str(album) and '"' not in str(title):
         try:
             engine.execute(f"""
-                    INSERT INTO scrobbling (uts, artist, artist_mbid, album, album_mbid, title, track_mbid)
-                    VALUES ({uts}, "{artist}", "{artist_mbid}", "{album}", "{album_mbid}", "{title}","{track_mbid}");
+                    INSERT INTO scrobbling (uts, artist, artist_mbid, album, album_mbid, title, track_mbid, fechahora)
+                    VALUES ({uts}, "{artist}", "{artist_mbid}", "{album}", "{album_mbid}", "{title}","{track_mbid}","{fechahora}");
                     """)
         except:
             print(f'{uts} sin comillas en ningún lugar')    
@@ -46,8 +65,8 @@ def insert_data(uts, artist, artist_mbid, album, album_mbid, title, track_mbid):
     elif '"' in str(artist) and '"' not in str(album) and '"' not in str(title):
         try:
             engine.execute(f"""
-                    INSERT INTO scrobbling (uts, artist, artist_mbid, album, album_mbid, title, track_mbid)
-                    VALUES ({uts}, '{artist}', "{artist_mbid}", "{album}", "{album_mbid}", "{title}","{track_mbid}");
+                    INSERT INTO scrobbling (uts, artist, artist_mbid, album, album_mbid, title, track_mbid, fechahora)
+                    VALUES ({uts}, '{artist}', "{artist_mbid}", "{album}", "{album_mbid}", "{title}","{track_mbid}","{fechahora}");
                     """)
         except:
             print(f'{uts} con comillas en artista')    
@@ -55,8 +74,8 @@ def insert_data(uts, artist, artist_mbid, album, album_mbid, title, track_mbid):
     elif '"' not in str(artist) and '"' in str(album) and '"' not in str(title):
         try:
             engine.execute(f"""
-                    INSERT INTO scrobbling (uts, artist, artist_mbid, album, album_mbid, title, track_mbid)
-                    VALUES ({uts}, "{artist}", "{artist_mbid}", '{album}', "{album_mbid}", "{title}","{track_mbid}");
+                    INSERT INTO scrobbling (uts, artist, artist_mbid, album, album_mbid, title, track_mbid, fechahora)
+                    VALUES ({uts}, "{artist}", "{artist_mbid}", '{album}', "{album_mbid}", "{title}","{track_mbid}","{fechahora}");
                     """)
         except:
             print(f'{uts} con comillas en album')    
@@ -64,8 +83,17 @@ def insert_data(uts, artist, artist_mbid, album, album_mbid, title, track_mbid):
     elif '"' not in str(artist) and '"' not in str(album) and '"' in str(title):
         try:
             engine.execute(f"""
-                    INSERT INTO scrobbling (uts, artist, artist_mbid, album, album_mbid, title, track_mbid)
-                    VALUES ({uts}, "{artist}", "{artist_mbid}", "{album}", "{album_mbid}", '{title}',"{track_mbid}");
+                    INSERT INTO scrobbling (uts, artist, artist_mbid, album, album_mbid, title, track_mbid, fechahora)
+                    VALUES ({uts}, "{artist}", "{artist_mbid}", "{album}", "{album_mbid}", '{title}',"{track_mbid}","{fechahora}");
+                    """)
+        except:
+            print(f'{uts} con comillas en title')    
+            next
+    elif '"' not in str(artist) and '"'  in str(album) and '"' in str(title):
+        try:
+            engine.execute(f"""
+                    INSERT INTO scrobbling (uts, artist, artist_mbid, album, album_mbid, title, track_mbid, fechahora)
+                    VALUES ({uts}, "{artist}", "{artist_mbid}", '{album}', "{album_mbid}", '{title}',"{track_mbid}","{fechahora}");
                     """)
         except:
             print(f'{uts} con comillas en title')    
@@ -120,15 +148,36 @@ def insert_newalb(Artist, Album, Title, Track, released, secs, kbs, creado, ruta
             print(f'{archivo} con comillas en title')    
             next
 
-# def insert_scro():
-#     ## llama al procedimiento almacenado "insert_scro (función de la base de datos de mysql y lo ejecuta)
-#     # engine.execute('call insert_scro();')
-#     #return pd.read_sql_query('call insert_Scro();', engine)
-#     engine.execute('call insert_scro;')
-#     return "ejecutado"
+
+def actual_error():
+
+
+    engine.execute("update scrobbling set artist = 'Juan Luis Guerra y 4.40' where album = 'Bachata Rosa' and artist = 'Juan Luis Guerra';")
+    engine.execute("update scrobbling set artist = 'Robe' where artist = 'Robe.';")
+    engine.execute("""
+    update scrobbling set title = "Can't Hold Us Down (ft. Lil' Kim)" where artist = 'Christina Aguilera' and title = "Can't Hold Us Down";
+    """)
+    engine.execute("""
+    update scrobbling set title = 'Satisfaction' where title = "(I Can't Get No) Satisfaction" and album = 'Out of Our Heads';
+    """)
+    engine.execute("update scrobbling set artist = 'Loquillo Y Trogloditas' where artist = 'Loquillo Y Los Trogloditas';")
+    engine.execute("update scrobbling set album = '[Desconocido]' where album = '' and artist in ('Rob Thomas','funk2maka','Jordi Estévez','Elena Bugedo','Joaquín Calderón','Zahara','Los Capos de México','Julio Muñoz');")
+    engine.execute("update scrobbling set title = 'La Senda Del Tiempo (directo)' where title = 'La Senda Del Tiempo' and album = 'Nos vemos en los bares';")
+    engine.execute("update scrobbling set title = 'Shake It Out (Unplugged)' where title = 'Shake It Out (Acoustic)' and album  = 'MTV Unplugged';")
+    engine.execute("""
+    update scrobbling set title = "You've Got the Love (Fraser T. Smith's Mix)" where title = "You've Got The Love (Fraser T Smith's Mix)" and album = 'Lungs';
+    """)
+    engine.execute("update scrobbling set title = 'Time After Time (Live)' where title = 'Time After Time (Live) (Live Australia)' and album = 'Live From Australia' and artist = 'Matchbox Twenty';")
+    engine.execute("update scrobbling set album = 'Dúos, Tríos Y Otras Perversiones' where album = 'Duos, trios y otras perversiones' and artist = 'Ariel Rot';")
+    engine.execute("update scrobbling set title = 'Me Estás Atrapando Otra Vez (Con M-Clan)' where title = 'Me Estás Atrapando Otra Vez' and album = 'Dúos, Tríos Y Otras Perversiones';")
+    engine.execute("update scrobbling set title = 'Mi Alma Vuela En Silencio (Rumba)' where title = 'Mi Alma Vuela En Silencio' and artist = 'Rosario La Tremendita';")
+    engine.execute("update scrobbling set title = 'We Go Together' where title ='We Go Together (© ¤ @)' and album = 'Grease [Original Soundtrack]';")
+
 
 def act_scro():
     last_uts = list(engine.execute(f'select max(uts) from scrobbling where id_can is not null;'))[0][0]
+
+    time.sleep(2)
     cuenta = list(engine.execute(f'''
     select count(uts) from scrobbling where concat(artist, album, title) not in (select completo from total)
     '''))[0][0]
@@ -138,18 +187,50 @@ def act_scro():
             set sc.id_Can = tt.id_Can
             where sc.id_Can is null;
             ''')
+        filas = list(engine.execute(f'select count(*) as  "Filas Actualizadas"from scrobbling where uts > {last_uts};'))[0][0]
+        print(f'Filas actualizadas: {filas}')
         return pd.read_sql_query(f'''
-            select count(*) as  "Filas Actualizadas"from scrobbling where uts > {last_uts};
+            select * from scrobbling where uts > {last_uts};
             
             ''',engine)
     else:
-        return pd.read_sql_query(f'''
-            select * from scrobbling where concat(artist, album, title) not in (select completo from total);
-            ''',engine)
+        print('corrigiendo algunos errores de insertación')
+        actual_error()
+        cuenta_again = list(engine.execute(f'''
+                select count(uts) from scrobbling where concat(artist, album, title) not in (select completo from total)
+                '''))[0][0]
+        if cuenta_again == 0:
+            engine.execute(f'''
+                update scrobbling sc join total tt on tt.completo = concat(sc.artist,sc.album,sc.title)
+                set sc.id_Can = tt.id_Can
+                where sc.id_Can is null;
+                ''')
+            filas = list(engine.execute(f'select count(*) as  "Filas Actualizadas"from scrobbling where uts > {last_uts};'))[0][0]
+            print(f'Filas actualizadas: {filas}')
+            return pd.read_sql_query(f'''
+                select * from scrobbling where uts > {last_uts};
+                
+                ''',engine)
+        else:
+            print('los siguientes errores no se han podido corregir')
+            print('(no se insertarán ninguno de los nuevos scrobbles hasta corregir erroes, stay tuned)')
+                
+            errores =  pd.read_sql_query(f'''
+                select * from scrobbling where concat(artist, album, title) not in (select completo from total);
+                ''',engine)
+            
+            engine.execute(f'delete from scrobbling where uts > {last_uts}')
 
-def act_uts():
-    engine.execute("CALL act_uts();")
-    return 'fechahora cambiada'
+            return errores
+            
+
+
+def checkuts(uts_):
+    cuenta = list(engine.execute(f"select count(*) from scrobbling where uts = {uts_}"))[0][0]
+    if cuenta == 0:
+        return False
+    else:
+        return True
 
 
     
