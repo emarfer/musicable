4af7c347310bfcbea31ec3f9a9102449e851d028
emarfer <estermarferironhack@gmail.com>
2022-11-16 19:36:09 +0100

    mongueando 🌿

--- mong.py
+++ mong.py
@@ -1,11 +1,11 @@
 import src.mongofun as mong # importa el módulo mongofun.py
 import src.cleansing as cls # importa el módulo cleansing.py
 
-lastuser = 'AveKaesar' # usuario de last.fm
+lastuser = 'aveKaesar' # usuario de last.fm
 print(f'importando datos de lastfm del usuario {lastuser}' ) # imprime en pantalla el mensaje
 mong.mongouser(lastuser) # inserta los datos de los scrobbles en la base de datos (mongouser() es una función definida en mongofun.py)
 
-lastuser = 'Sinatxester' # usuario de last.fm
+lastuser = 'sinatxester' # usuario de last.fm
 print(f'importando datos de lastfm del usuario {lastuser}') # imprime en pantalla el mensaje
 mong.mongouser(lastuser) # inserta los datos de los scrobbles en la base de datos (mongouser() es una función definida en mongofun.py)
 
@@ -14,7 +14,8 @@ if masusuarios == False: # si no hay nuevos usuarios en la base de datos
     print('Hasta luego Maricarmen') # imprime en pantalla el mensaje
 
 elif masusuarios == True: # si hay nuevos usuarios en la base de datos
-    print(masusuarios, 'prueba') # imprime en pantalla el número de nuevos usuarios
-    usuario = input('¿Qué usuario quieres insertar?: \n') # pide al usuario que introduzca el nombre de un usuario
+    # print(masusuarios, 'prueba') # imprime en pantalla el número de nuevos usuarios
+    usuario_imput = input('¿Qué usuario quieres insertar?: \n') # pide al usuario que introduzca el nombre de un usuario
+    usuario = usuario_imput.lower()
     print(f'importando datos de lastfm del usuario {usuario}' ) # imprime en pantalla el mensaje
     mong.mongouser(usuario) # inserta los datos de los scrobbles en la base de datos (mongouser() es una función definida en mongofun.py)
