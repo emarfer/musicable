b8fba8d540d2b4e6dc4daeec6cf30a84b8306bf2
emarfer <emarferironhack@gmail.com>
2023-02-15 22:53:23 +0100

    stash

--- src/sqltools.py
+++ src/sqltools.py
@@ -151,6 +151,11 @@ def actual_error():
     engine.execute("""
                    update scrobbling set title = "Bitch I'm Loca (ft. Maluma)"  where artist = 'Madonna' and album = 'Madame X' and title = "Bitch I’m Loca (ft. Maluma)"
                    """)
+    engine.execute("""
+                   update scrobbling set album = "La fabulosa historia de…"  where album = 'La fabulosa historia de∑' and artist = 'Zahara'
+                   """)
+    # La fabulosa historia de∑ 
+    
 
 
 
