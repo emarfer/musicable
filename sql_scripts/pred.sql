916f6b48e52b1ffebe2cb6496d3ca01319ac2cb6
emarfer <estermarferironhack@gmail.com>
2023-04-15 16:30:06 +0200

    predictions file updated 🔮

--- sql_scripts/pred.sql
+++ sql_scripts/pred.sql
@@ -4,8 +4,8 @@ use musicablecero;
     -- segundo parámetro anual o estación si la primera lista la queremos de año o de esatción. normalmente seleccionaremos estación.
 
 #lista de datos es de cumple o de año    
- -- set @datos = 'cumple'; 
-set @datos = 'anual';     
+ set @datos = 'cumple'; 
+-- set @datos = 'anual';     
 
 #estamos baremando primero la estación o el año
 set @estacional = 'estacion';
@@ -19,7 +19,7 @@ select * from prediction where veces > 0;
 select * from prediction where veces = 0;
 
 select min(rep) into @minrep from prediction;
-select @minrep; -- 2
+select @minrep; -- 1
 
 select * from prediction where rep = @minrep;
 
@@ -30,15 +30,20 @@ select id_can, artist, album, title, genero, sexo, rep, veces from prediction
 union
 select id_can, artist, album, title, genero, sexo, rep, veces from base_pred
 	where id_art in (select id_art from prediction where veces > 0) and veces = 0
-		 and rep > (@minrep - 3)
+		 and rep > (@minrep - 2)
 order by artist, rep desc;
--- -2: 49 // -3: 25 46 29 30 39 11 7 4 0!!!
+-- 1: 59 19 2:29 21 12 14 32 16 15 11 10 8 16 
+
+
+
+
 
 select id_can, artist, album, title, genero, sexo, rep, veces from base_pred 
 where id_art not in (Select id_art from prediction
 					-- where veces = 0
+                    -- and rep >= @minrep+2
 													)
-	and rep >= (@minrep - 2)
+	-- and rep >= (@minrep - 3)
 	-- and sexo = 'fem'
 	and veces = 0
   --  and id_can not in (select id_can from prediction_dos)
@@ -51,7 +56,7 @@ select * from prediction_dos where veces > 0 or prec is true;
 select * from prediction_dos where veces = 0 and prec is false; -- 25 33 35
 
 select min(rep) into @minrepdos from prediction_dos;
-select @minrepdos; -- 6
+select @minrepdos; -- 3
 select * from prediction_dos where rep = @minrepdos; 
 select sum(secs)/60 from prediction_dos where rep = @minrepdos;
 select * from prediction_dos where rep = @minrepdos and veces = 0 and prec = 0; 
@@ -67,17 +72,18 @@ select id_can, artist, album, title, genero, sexo, rep, veces,prec  from base_pr
         and prec is false
 		and rep >= (@minrepdos - 1)
 order by artist, rep desc, prec desc; 
--- -1:139 127 125 119 118 113 112 113
-
+-- -1:312 360 316 322 333 322 373 // 3 // 120 119 // 159
+ 
 
 select id_can, artist, album, title, genero, sexo, rep, veces, prec from base_pred_dos 
 where id_art not in (Select id_art from prediction_dos 
 				-- where veces = 0
 					)
     and prec is false
-	 and rep >= (@minrepdos -2)
+	-- and rep >= (@minrepdos -2)
    -- and sexo = 'fem'
 and veces = 0;
+-- 196  203
 
 
 -- ----------------------------
@@ -89,7 +95,7 @@ call temas_lista (@artist);
 
 -- --------------------------------
 set @reptot = 50;
-call albums_sin_rep (@reptot); -- 40:10 30:21 40:9 30:19
+call albums_sin_rep (@reptot); -- 40:10 30:21 40:9 30:19 40:7 30:17
 
 
 
@@ -98,7 +104,28 @@ select a.artist, count(l.id_can) as cuenta from artistas a left join temas t on 
 where a.id_art in (select id_art from temas where id_can in (select id_can from temasenlistados))
 group by a.artist order by cuenta desc;
 
-select count(*) from biblioteca;
-select * From biblioteca
+
+select * from total 
+where id_can not in (select distinct  id_can from listas_rep)
+and artist = 'Arctic Monkeys'
+and id_can in (select id_can from base_pred)
+order by rep desc
 ;
-select 22243 -22389;
+
+select id_can, artist, album, title, genero, sexo, rep, veces, prec from base_pred_dos 
+where id_art not in (Select id_art from prediction_dos 
+				-- where veces = 0
+					)
+    and prec is false
+	and rep >= (@minrepdos -1)
+   -- and rep = @minrepdos
+   -- and sexo = 'fem'
+and veces = 0
+and id_Can not in (select id_can from base_pred)
+;
+
+-- ---- 
+
+select min(id_pre) from prediction where veces > 0 and rep =  @minrep + 1; -- 11
+select sum(secs)/60 From prediction where id_pre >= 11;
+
